0595b18f71e7f5f12594015d574d2c0b3291529b
rocio <contactolda@gmail.com>
2020-03-12 15:31:27 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="main">
+        <div v-for="(personaje,index) in personajes" :key="index">
+            <TarjetaPersonaje v-bind:nombre="personaje.name" v-bind:imagen="personaje.image" />
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import TarjetaPersonaje from "./TarjetaPersonaje.vue"
+    export default {
+        name: "App",
+        components: {
+            TarjetaPersonaje
+        },
+        data() {
+            return {
+                personajes: [{
+                        name: 'Rick Sanchez',
+                        image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+
+                    },
+                    {
+                        name: 'Morty Smith',
+                        image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+
+                    },
+                    {
+                        name: 'Summer Smith',
+                        image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+
+                    },
+                    {
+                        name: 'Beth Smith',
+                        image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+
+                    },
+                    {
+                        name: 'Jerry Smith"',
+                        image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+
+                    },
+
+                ]
+            }
+        },
+
+    }
+</script>
+
+<style>
+*{
+    padding: 0;
+    margin: 0;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+}
+
+#main {
+    background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+}
+
+
+</style>
---
+++ src/TarjetaPersonaje.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="edicion">
+  
+    <img :src= 'imagen'/>
+      <p>{{nombre}}</p>
+</div>
+</template>
+
+<script>
+export default {
+     name: "TarjetaPersonaje",
+     props: {
+        nombre: String,
+        imagen: String
+} }
+</script>
+
+<style>
+
+.edicion {
+    display: flex;
+    align-items: center;
+    margin-left: 3rem;
+    padding-top: 2rem;
+}
+p {
+    color: green;
+    font-weight: 700;
+    font-size: 18px;
+    margin-left: 2rem;
+  
+}
+
+img {
+    border-radius: 50%;
+    width: 100px;
+   
+
+}
+</style>
+
